22b882b19b5aaa085cfc13962beb5e7d2ad46df1
BuildTools <quarktheawesome@gmail.com>
2016-04-07 20:19:02 +1000

    Throw in basic template

---
+++ demo.c
@@ -0,0 +1,16 @@
+/*
+Make Me A Heap v1.0 - A Wii U library to make large heaps in a dirty userspace environment.
+
+Distributed under DBaJ 1.2; please see https://github.com/QuarkTheAwesome/MakeMeAHeap/raw/master/LICENSE.md for more details.
+
+This file is part of the demo to show the library's usage. It should be ran with yellows8's browserhax exploit.
+*/
+
+void demoMain() {
+	
+}
+
+void _start() {
+	//Crazy stuff for the WiiU to start here
+	demoMain();
+}
---
+++ heapcreate.c
@@ -0,0 +1,9 @@
+/*
+Make Me A Heap v1.0 - A Wii U library to make large heaps in a dirty userspace environment.
+
+Distributed under DBaJ 1.2; please see https://github.com/QuarkTheAwesome/MakeMeAHeap/raw/master/LICENSE.md for more details.
+
+This file is part of the library itself.
+*/
+
+void;
